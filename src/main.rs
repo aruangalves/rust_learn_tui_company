a5fb41b6a66148ee2444cba1d1569142199bf0ee
Aruan Galves <aruangalves@gmail.com>
2026-06-15 10:45:18 -0300

    Preparing menu entries

--- src/main.rs
+++ src/main.rs
@@ -19,5 +19,40 @@ fn main() {
         io::stdin()
             .read_line(&mut user_input)
             .expect("Failed to read line");
+
+        //splitting user input into a Vector
+        let split_input: Vec<String> = user_input.split(' ').map(|s| s.to_string()).collect();
+
+        //guarantees that somehow the input wasn't empty
+        //so I can safely retrive the first element
+        if split_input.is_empty() {
+            println!("Invalid input, please try again.");
+            continue;
+        }
+
+        let first_input: &String = split_input.first().unwrap();
+        let first_input = first_input.clone().to_lowercase();
+
+        //Test ok, first word is lowercased...
+        //println!("The first input is {}", first_input);
+
+        match first_input.as_str() {
+            "add" => {
+                println!("TODO: add new employee to hashmap...");
+            }
+            "list" => {
+                println!("TODO: list employees by department...");
+            }
+            "exit" => break,
+            _ => {
+                println!("Invalid input, please try again.");
+            }
+        }
+
+        println!("The word count is {}", split_input.len());
+
+        for s in &split_input {
+            println!("{}", s);
+        }
     }
 }
